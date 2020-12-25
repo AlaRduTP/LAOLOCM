9e6bbeb80d3c0a5370d94f01370157740f722876
AlaRduTP <alardutp37259418@gmail.com>
2020-12-25 22:16:17 +0800

    add fitness initialize and finalize function

--- fitness.cpp
+++ fitness.cpp
@@ -1,13 +1,18 @@
-#include <Python.h>
-
 #include "fitness.h"
 #include "caller.h"
 
-float fitness(std::vector<std::string> & genes) {
-    PyImport_AppendInittab("caller", PyInit_caller);
+int fitness_initialize() {
+    int ret = PyImport_AppendInittab("caller", PyInit_caller);
     Py_Initialize();
+    return ret;
+}
+
+float fitness(std::vector<std::string> & genes) {
     PyImport_ImportModule("caller");
     float ret = call_fitness(genes);
-    Py_Finalize();
     return ret;
+}
+
+void fitness_finalize() {
+    Py_Finalize();
 }
--- fitness.h
+++ fitness.h
@@ -1,4 +1,11 @@
+#ifndef FITNESS
+#define FITNESS
+
 #include <vector>
 #include <string>
 
+int fitness_initialize();
 float fitness(std::vector<std::string> & genes);
+void fitness_finalize();
+
+#endif
