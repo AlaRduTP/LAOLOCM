680d4365f2fb5d59d0ef29a9f6287fb998b5ae54
skps2010 <skps2010030908@yahoo.com>
2021-01-19 10:30:59 +0800

    crash games will get low points

--- show.py
+++ show.py
@@ -2,20 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sys import stdin, argv
 
-data = []
+x = []
+y = []
 file = stdin
 if len(argv) == 2:
     file = open(argv[1], "r")
 
+gen = 1
 for line in file:
     if 'Fitness' in line:
-        data.append(list(map(float, line.split()[1:])))
+        fitnesses = line.split()[1:]
+        x.append([gen] * len(fitnesses))
+        y.append(list(map(float, fitnesses)))
+        gen += 1
 
 plt.figure(figsize=(10, 7))
 plt.title('Agent Fitness')
 plt.ylabel('Fitness')
 plt.xlabel('Generation')
-plt.boxplot(data)
-loc = np.arange(1, len(data) + 1, len(data) // 30 + 1)
-plt.xticks(loc, loc)
+plt.plot(x, y, '.', color='blue')
 plt.show()
